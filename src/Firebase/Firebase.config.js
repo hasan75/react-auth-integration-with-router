af5c0acbbc26b593bc8e174b1d259b409f3ba790
Hasan75 <hasanjab14@gmail.com>
2021-10-09 02:20:59 +0600

    added firebase authentication

---
+++ src/Firebase/Firebase.config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyABzZmyvSer-PZQuc1DzUNrVn3QIyNwNDw',
+  authDomain: 'simple-firebase-authenti-23b78.firebaseapp.com',
+  projectId: 'simple-firebase-authenti-23b78',
+  storageBucket: 'simple-firebase-authenti-23b78.appspot.com',
+  messagingSenderId: '1076293186585',
+  appId: '1:1076293186585:web:8f8e1db8d60fa79014a688',
+  measurementId: 'G-LRST3GS6SJ',
+};
+
+export default firebaseConfig;
