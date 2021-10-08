76d36b478833b3a1e47c42c698d80658d7062d54
Hasan75 <hasanjab14@gmail.com>
2021-10-09 02:54:13 +0600

    on SIgnIn button OnClick handler added

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -7,7 +7,7 @@ const Login = () => {
   return (
     <div>
       <h2>Please Login</h2>
-      <button>Google Sign In</button>
+      <button onClick={signInUsingGoogle}>Google Sign In</button>
       <br />
       <Link to='/register'>New User?</Link>
     </div>
