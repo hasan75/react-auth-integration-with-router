26a99ace396978e065aba33ab00566b92f8d2d42
Hasan75 <hasanjab14@gmail.com>
2021-10-09 02:53:33 +0600

    in login.js useFirebase hooks used and destructured for connecting login to googleSignIn

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import useFirebase from '../hooks/useFirebase';
 
 const Login = () => {
+  const { signInUsingGoogle } = useFirebase;
   return (
     <div>
       <h2>Please Login</h2>
